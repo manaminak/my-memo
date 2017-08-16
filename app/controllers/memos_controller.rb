7262618838471f23dce3c0cd6b2ffe417ceb0172
manami <manami.a6@keio.jp>
2017-08-16 08:15:11 +0000

    Add price / Modify time

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -69,6 +69,6 @@ class MemosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def memo_params
-      params.require(:memo).permit(:date, :time, :memo)
+      params.require(:memo).permit(:created_at, :price, :memo)
     end
 end
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,2 +1,4 @@
 class Memo < ApplicationRecord
+ validates :price , length: {maximum: 16} , presence: true
+ validates :memo , length: {maximum: 256} , presence: true
 end
